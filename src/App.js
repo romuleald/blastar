a5a50ad381d2f31980cce5246d394a4892dd08c4
Antoine Sanchez <antoine.sanchez@axa.fr>
2019-09-19 21:50:58 +0200

    build: First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const Card = ({title}) =>
+    <div className="card">{title}</div>;
+
+const Cards = ({cards}) =>
+    <div className="cards">
+        {cards.map(title => <Card title={title}/>)}
+    </div>;
+
+const Player = ({playerName, cards}) =>
+    <li className="player">
+        <h3>{playerName}</h3>
+        <Cards cards={cards}/>
+    </li>;
+
+const Players = ({players}) =>
+    <ul>
+        {players.map(({playerName, cards}) => <Player playerName={playerName} cards={cards}/>)}
+    </ul>;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>Title/Header</header>
+            <h2>Adversaires</h2>
+            <div className="players">
+                <Players players={[{playerName: 'kikoo', cards: ['Carte 1', 'Carte 2']}]}/>
+            </div>
+            <Player playerName={"YOU"} cards={['Carte 1']} />
+        </div>
+    );
 }
 
 export default App;
