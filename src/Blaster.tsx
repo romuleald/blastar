132d2972a587526069eb3641666fa84d6503caca
Antoine Sanchez <antoine.sanchez@axa.fr>
2019-10-09 21:07:21 +0200

    ci(game): Migrate to typescript
    * add prettier/eslint

--- src/Blaster.tsx
+++ src/Blaster.tsx
@@ -8,7 +8,7 @@ import background from './background.jpg';
 import {defaultPlayerName} from './constants/playerConstants';
 import CardViewer from './components/CardViewer';
 
-const AddNewPlayer = ({addPlayer}) => {
+const AddNewPlayer = ({addPlayerAction}) => {
     const [newPlayerName, setNewPlayerName] = React.useState(defaultPlayerName);
     return (
         <div>
@@ -16,43 +16,47 @@ const AddNewPlayer = ({addPlayer}) => {
             <input
                 type="text"
                 defaultValue={newPlayerName}
-                onChange={event => setNewPlayerName(event.target.value)} />
-            <button onClick={() => addPlayer(newPlayerName)}>Add Player</button>
+                onChange={event => setNewPlayerName(event.target.value)}
+            />
+            <button onClick={() => addPlayerAction(newPlayerName)}>
+                Add Player
+            </button>
         </div>
     );
 };
 
-const StartGame = ({startGame}) => (
+const StartGame = ({startGameAction}) => (
     <div>
         <h3>Start game</h3>
-        <button onClick={() => startGame()}>Start</button>
+        <button onClick={() => startGameAction()}>Start</button>
     </div>
 );
 
-const Blaster = ({addPlayer, startGame, isGameStarted}) => { 
-    return (
-        <div className="App">
-            <img className="game-background" src={background} alt="background" />
-            <header>Title/Header</header>
-            <div className="dev-helpers">
-                <h2>Dev Helpers</h2>
-                <AddNewPlayer addPlayer={addPlayer} />
-                <StartGame startGame={startGame} />
-                <HelperSelect />
-            </div>
-            {isGameStarted && <GameBoard />}
-            <CardViewer />
+const Blaster = ({addPlayerAction, startGameAction, isGameStarted}) => (
+    <div className="App">
+        <img className="game-background" src={background} alt="background" />
+        <header>Title/Header</header>
+        <div className="dev-helpers">
+            <h2>Dev Helpers</h2>
+            <AddNewPlayer addPlayerAction={addPlayerAction} />
+            <StartGame startGameAction={startGameAction} />
+            <HelperSelect />
         </div>
-    );
-};
+        {isGameStarted && <GameBoard />}
+        <CardViewer />
+    </div>
+);
 
 const mapStateToProps = state => ({
     isGameStarted: state.PLAYERS.isGameStarted
 });
 
 const mapDispatchToProps = dispatch => ({
-    addPlayer: (playerName) => dispatch(addPlayer({name: playerName})),
-    startGame: () => dispatch(startGame())
-})
+    addPlayerAction: playerName => dispatch(addPlayer({name: playerName})),
+    startGameAction: () => dispatch(startGame())
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Blaster);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Blaster);
--- src/store.ts
+++ src/store.ts
@@ -6,10 +6,16 @@ import {gameMiddleware} from './middlewares/gameMiddleware';
 
 const middleware = [thunk, gameMiddleware];
 
+// eslint-disable-next-line no-undef
 if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const {createLogger} = require('redux-logger');
     const logger = createLogger({collapsed: true});
     middleware.push(logger);
 }
 
-export const makeStore = () => createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
+export const makeStore = () =>
+    createStore(
+        rootReducer,
+        composeWithDevTools(applyMiddleware(...middleware))
+    );
